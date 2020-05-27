c33d47a77093963e0eb22ad2e3c4112c0155b09a
Masataka Mimuro <masataka.mimuro@gmail.com>
2020-05-27 17:01:00 +0900

    Add &, |, ^, &=, |= and ^=

--- zcc.h
+++ zcc.h
@@ -74,6 +74,9 @@ typedef enum {
     ND_MUL,       // *
     ND_DIV,       // /
     ND_MOD,       // % modulo
+    ND_BITAND,    // &
+    ND_BITOR,     // |
+    ND_BITXOR,    // ^
     ND_EQ,        // ==
     ND_NE,        // !=
     ND_LT,        // < less than
