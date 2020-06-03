20639f46e5147812400f182090c88a74c657d6ad
Masataka Mimuro <masataka.mimuro@gmail.com>
2020-06-03 09:16:04 +0900

    update .gitignore and add self.sh test/extern.c

---
+++ self.sh
@@ -0,0 +1,58 @@
+#!/bin/bash -x
+set -e
+
+TMP=tmp-self
+
+mkdir -p $TMP
+
+zcc() {
+    cat <<EOF > $TMP/$1
+typedef struct FILE FILE;
+extern FILE *stdin;
+extern FILE *stdout;
+extern FILE *stderr;
+
+void *malloc(long size);
+void *calloc(long nmemb, long size);
+void *realloc(void *buf, long size);
+int *__errno_location();
+char *strerror(int errnum);
+FILE *fopen(char *pathname, char *mode);
+long fread(void *ptr, long size, long nmemb, FILE *stream);
+int fclose(FILE *fp);
+int feof(FILE *stream);
+static void assert() {}
+int strcmp(char *s1, char *s2);
+int printf(char *fmt, ...);
+int sprintf(char *buf, char *fmt, ...);
+long strlen(char *p);
+int strncmp(char *p, char *q);
+void *memcpy(char *dst, char *src, long n);
+char *strndup(char *p, long n);
+int isspace(int c);
+char *strstr(char *haystack, char *needle);
+EOF
+
+    grep -v '^#' zcc.h >> $TMP/$1
+    grep -v '^#' $1 >> $TMP/$1
+    sed -i 's/\bbool\b/_Bool/g' $TMP/$1
+    sed -i 's/\berrno\b/*__errno_location()/g' $TMP/$1
+    sed -i 's/\btrue\b/1/g; s/\bfalse\b/0/g;' $TMP/$1
+    sed -i 's/\bNULL\b/0/g' $TMP/$1
+    sed -i 's/INT_MAX/2147483647/g' $TMP/$1
+
+    ./zcc $TMP/$1 > $TMP/${1%.c}.s
+    gcc -c -o $TMP/${1%.c}.o $TMP/${1%.c}.s
+}
+
+cc() {
+    gcc -c -o $TMP/${1%.c}.o $1
+}
+
+zcc main.c
+zcc type.c
+cc parse.c # need "unsigned" keyword
+zcc codegen.c
+cc tokenize.c
+
+gcc -static -o zcc-stage2 $TMP/*.o
