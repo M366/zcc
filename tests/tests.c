4c742296f2b2864fc8eaedadca28d4f2e04ecb52
Masataka Mimuro <masataka.mimuro@gmail.com>
2020-06-09 23:41:09 +0900

    Add #undef

--- tests/tests.c
+++ tests/tests.c
@@ -1277,6 +1277,8 @@ int main() {
   assert(3, M1, "M1");
 #define M1 4
   assert(4, M1, "M1");
+#undef M1
+  assert(5, M1, "M1");
 
 #define M1 3+4+
   assert(12, M1 5, "M1 5");
@@ -1289,6 +1291,10 @@ int main() {
 #define five "5"
 #define END )
   ASSERT 5, if, five END;
+#undef ASSERT
+#undef if
+#undef five
+#undef END
 
   printf("OK\n");
   return 0;
